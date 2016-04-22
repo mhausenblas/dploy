bfc318598c50898a708328ef05f316fd5e9502ce
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-04-22 22:06:44 +0100

    inits logging and testing

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"os"
 )
 
 var version = "0.1.0"
+var logger = log.New(os.Stdout, "[DPLOY]: ", log.Ldate|log.Ltime)
+var verboseReporting = false
+
+func deploy_init() {
+	logger.Printf("In init")
+}
 
 func main() {
 	beVerbose := flag.Bool("v", false, "Be verbose, defaults to false.")
-	cmd := os.Args[1:]
-	fmt.Printf("This is dploy version %s\n", version)
-	fmt.Printf("Executing command %s\n", cmd)
+	cmd := os.Args[1:1]
+	logger.Printf("This is dploy version %s\n", version)
+	logger.Printf("Executing command %s\n", cmd)
 	if *beVerbose {
-		fmt.Printf("I will be verbose.")
+		verboseReporting = true
 	}
+	deploy_init()
 }
