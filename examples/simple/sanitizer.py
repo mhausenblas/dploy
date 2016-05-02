582ce432a4708b040ddb746723076ccc8897b462
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-05-02 11:06:38 +0100

    makes sanitizer Python 3

--- examples/simple/sanitizer.py
+++ examples/simple/sanitizer.py
@@ -12,12 +12,9 @@ def sanitize():
     service_name = "buzzgen-dployex.marathon.mesos"
     components = service_name.split('.')
     lookup = "_" + components[0] + "._tcp." + ".".join(str(x) for x in components[1:])
-    print lookup
     payload = json.load(urllib2.urlopen(mesosdns_endpoint + "/v1/services/" + lookup + "."))
-    print payload
     service_instance = random.choice(payload)
     bg_service = "http://" + service_instance['ip'] + ":" + str(service_instance['port'])
-    print bg_service
     response = urllib2.urlopen(bg_service)
     content = response.read()
     return content*10
