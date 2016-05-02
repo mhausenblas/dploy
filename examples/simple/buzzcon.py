a7714b1669d294ae8c27bef152a466f423fbfd84
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-05-02 12:29:14 +0100

    fixes buzz consumer

--- examples/simple/buzzcon.py
+++ examples/simple/buzzcon.py
@@ -9,22 +9,26 @@ class BuzzConsumerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         mesosdns_endpoint = "http://leader.mesos:8123"
         service_name = "buzzgen-dployex.marathon.mesos"
-        components = service_name.split('.')
+        components = service_name.split(".")
         lookup = "_" + components[0] + "._tcp." + ".".join(str(x) for x in components[1:])
         response = urllib.request.urlopen(mesosdns_endpoint + "/v1/services/" + lookup + ".")
-        str_response = response.read().decode('utf-8')
+        str_response = response.read().decode("utf-8")
         payload = json.loads(str_response)
         service_instance = random.choice(payload)
-        bg_service = "http://" + service_instance['ip'] + ":" + str(service_instance['port'])
+        bg_service = "http://" + service_instance["ip"] + ":" + str(service_instance["port"])
         response = urllib.request.urlopen(bg_service)
         content = response.read()
-        return content*10
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*") # enable CORS
+        self.end_headers()
+        self.wfile.write(content*10)
+        return 
 
 
 ################################################################################
 # Main script
 #
-if __name__ == '__main__':
+if __name__ == "__main__":
     from http.server import HTTPServer
-    server = HTTPServer(('', 8888), BuzzConsumerHandler)
+    server = HTTPServer(("", 8888), BuzzConsumerHandler)
     server.serve_forever()
