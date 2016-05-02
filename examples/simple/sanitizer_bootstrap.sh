1cf136c3e47a62ed415cb78317ebb99be916dfe0
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-05-02 09:20:47 +0100

    fixes bootstrap

--- examples/simple/sanitizer_bootstrap.sh
+++ examples/simple/sanitizer_bootstrap.sh
@@ -3,4 +3,5 @@
 bg_port=$(dig _buzzgen-dployex._tcp.marathon.mesos SRV | grep ^_buzzgen-dployex._tcp.marathon.mesos. | cut -d " " -f 6)
 bg_addsec=$(dig _buzzgen-dployex._tcp.marathon.mesos SRV | grep ^_buzzgen-dployex._tcp.marathon.mesos. | cut -d " " -f 7)
 bg_ip=$(dig _buzzgen-dployex._tcp.marathon.mesos SRV | grep ^${bg_addsec} | cut -d " " -f 5)
-BGSERVER=$bg_ip:$bg_port python sanitizer.py
+export 	BGSERVER=$bg_ip:$bg_port
+python sanitizer.py
